6a8bf4c21497a943879f75d5f81b4da2d34b6a26
R-Alin-Gabriel <rudareanu_alin@yahoo.com>
2022-03-24 15:48:20 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css"
+import useLocalStorage from "use-local-storage";
 
 function App() {
+
+ const [theme, setTheme] = useLocalStorage('theme' ? "dark" : "light")
+ const switchTheme = () =>{
+   const newTheme = theme === "light" ? "dark" : "light"
+   const x = document.getElementById('demo')
+   if (x.innerHTML === "Light Theme"){
+     x.innerHTML = "Dark Theme"
+   }else {
+     x.innerHTML ="Light Theme"
+   }
+
+   setTheme(newTheme)
+ }
+ 
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="app" data-theme={theme} >
+      <div className="login">
+        <h1>Login</h1>
+        <div className="container">
+          <div className="top">
+          <i class='fab fa-google'></i>
+            <i class='fab fa-facebook-square'></i>
+            <i class='fab fa-linkedin'></i>
+            <i class='fab fa-twitter-square'></i>
+            <i class='fab fa-apple'></i>
+          </div>
+          <p className='divider' ><span>Or</span></p>
+          <form action="">
+            <label htmlFor="">Email</label>
+            <input type="email" placeholder='Enter Your Email ' />
+            <label htmlFor="">Password</label>
+            <input type="password" placeholder='Enter Your Password ' />
+            <div className="remember">
+              <input type="checkbox"  />
+              <p>Remember Me</p>
+            </div>
+            <button>Log In</button>
+          </form>
+          <div className="bottom">
+            <p>Forget Your Password?</p>
+            <a href="/">Reset Password</a>
+          </div>
+          <p className='create' >Crate Account</p>
+        </div>
+        <div className="theme-toggle">
+          <h2 id="demo">Light Theme</h2>
+          <i onClick={switchTheme}  className='fas fa-toggle-on' ></i>
+        </div>
+      </div>
     </div>
+      
   );
 }
 
